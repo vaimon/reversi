13233357d90e174ddf2c60643e5716512d058e65
Andrew Vaimon <niko142000@mail.ru>
2021-10-29 09:45:11 +0300

    clean output

--- main.cpp
+++ main.cpp
@@ -20,7 +20,10 @@ int main(int argc, char *argv[]) {
         std::cout << std::endl << std::endl;
     }
     while (true) {
-        g.printSituation();
+        if(isBotFightMode){
+            g.printSituation();
+        }
+
         auto move = g.decideHowToMove();
         if (move == "skip") {
             std::cout << "Skip move " << std::endl;
@@ -35,13 +38,18 @@ int main(int argc, char *argv[]) {
             return 3;
         } else {
             g.makeBotMove(move, true);
-            std::cout << order << " Our move: " << move << std::endl;
+            if(isBotFightMode){
+                std::cout << order;
+            }
+            std::cout << " Our move: " << move << std::endl;
             if (isBotFightMode) {
                 std::cerr << move << std::endl;
             }
             g.printField();
             std::string state = g.checkForWin();
-            //std::cout << "state: " << state << std::endl;
+            if(isBotFightMode){
+                std::cout << "state: " << state << std::endl;
+            }
             if (state != "") {
                 if (state == "draw") {
                     std::cout << "Draw! " << std::endl;
@@ -63,11 +71,18 @@ int main(int argc, char *argv[]) {
             std::cout << "No moves, sweetie? Hahaha, my turn!" << std::endl;
             continue;
         }
-        std::cout << "I am " << order << " waiting for my dumb opponent..." << std::endl;
-        g.printSituation();
+        if(isBotFightMode){
+            std::cout << "I am " << order << " waiting for my dumb opponent..." << std::endl;
+        }
+        if(isBotFightMode){
+            g.printSituation();
+        }
         std::cin >> oppMove;
         g.makeBotMove(oppMove, false);
-        std::cout << order << " Opponent move: " << oppMove << std::endl;
+        if(isBotFightMode){
+            std::cout << order;
+        }
+        std::cout << " Opponent move: " << oppMove << std::endl;
         g.printField();
         std::cout << std::endl << std::endl;
 
